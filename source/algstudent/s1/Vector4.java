50ad0433748d7e2e09c3490f262a253cae7d4ab6
Pablo <Pablo@Kaka>
2021-02-16 15:04:51 +0100

    Update

---
+++ source/algstudent/s1/Vector4.java
@@ -0,0 +1,16 @@
+package algstudent.s1;
+
+public class Vector4 {
+	
+	public static void main(String args[]) {
+		int n = Integer.parseInt(args[0]);
+		int[] v = new int[n*240000];
+		Vector1.fillIn(v);
+		
+		long sTime = System.currentTimeMillis();
+		Vector1.sum(v);
+		long eTime = System.currentTimeMillis() - sTime;
+		
+		System.out.println("Elapsed time in ms: " + eTime);
+	}
+}
